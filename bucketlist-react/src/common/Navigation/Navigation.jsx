6cf0520ae86b7215580adfcb5e7e243402dd8f5f
Ace <rossie@Roses-MacBook-Pro.local>
2017-10-27 11:47:16 +0300

    [Feature #[151882709]]Registration page
    	-Add functionality for registering a user
    	-Update package.json

--- bucketlist-react/src/common/Navigation/Navigation.jsx
+++ bucketlist-react/src/common/Navigation/Navigation.jsx
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
+import {Button} from 'react-bootstrap';
 
 export class NavigationContainer extends Component {
     render(){
         return(
-            <div>Nav</div>
+            <div>
+                <Button type="submit" bsStyle="primary">
+                    Sign in
+                </Button>
+                <Button type="submit" bsStyle="primary">
+                    Login
+                </Button>
+            </div>
         )
     }
 }
